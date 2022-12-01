93dd9225d93151b227655444fec49d383c7b0943
JuanFlorez1326 <juanks339@gmail.com>
2022-12-01 16:35:07 -0500

    ADD: Petition: Seed with Axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `This action returns all seed`;
+  private readonly axios: AxiosInstance = axios
+
+  async executeSeed() {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+    const { data } = await this.axios.get<PokeResponse>(url)
+    data.results.forEach (({ name, url }) => {
+      const segments = url.split('/')
+      const no = +segments [segments.length - 2]
+      console.log({name, no});
+    })
+    return data.results;
   }
 }
