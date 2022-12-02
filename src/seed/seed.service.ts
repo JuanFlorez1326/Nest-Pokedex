07941a04c4c1d6c8cdda45217de1d46d9a8daffb
JuanFlorez1326 <juanks339@gmail.com>
2022-12-01 19:58:25 -0500

    NEW: Insert multiple pokemons at once

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
@@ -7,14 +10,40 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios
 
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
   async executeSeed() {
-    const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+    //Elimina todo de la base de datos.
+    await this.pokemonModel.deleteMany({})
+
+    //URL de la API
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=650'
+
+    //Obtiene los primeros 10 pokemones de la API con Axios
     const { data } = await this.axios.get<PokeResponse>(url)
-    data.results.forEach (({ name, url }) => {
+
+    //Array para guardar los pokemones en la base de datos
+    const pokemonToInsert: { name: string, no: number }[] = []
+
+    //Mapea los resultados de la API y los guarda en la base de datos
+    data.results.forEach ( ({ name, url }) => {
+
+      //Obtiene el número del pokemon
       const segments = url.split('/')
+
+      //Convertir el número a entero
       const no = +segments [segments.length - 2]
-      console.log({name, no});
+
+      //Guarda el pokemon en el array
+      pokemonToInsert.push({ name, no })
     })
-    return data.results;
+
+    //Guarda los pokemones en la base de datos
+    await this.pokemonModel.insertMany(pokemonToInsert)
+    return 'Seed executed';
   }
 }
